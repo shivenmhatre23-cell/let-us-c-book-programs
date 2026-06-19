d3b4b92473c04223f116f90cc5209d6085f2fee7
Shiven Mhatre <shivenmhatre23@gmail.com>
2026-06-19 13:54:56 +0530

    Using scanf and printf in a program

---
+++ Chapter 1/using_printf_and_scanf_(2nd).c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int num;
+    printf("Enter a number");
+    scanf("%d", &num);
+    printf("Now I am letting you on a secret...");
+    printf("You just entered the number %d", num);
+    
+}
