c701265e6e8fb7c8408e2589b07b4f2bed802bea
Shiven Mhatre <shivenmhatre23@gmail.com>
2026-06-25 15:20:18 +0530

    Used if loop in a program

---
+++ Chapter 2/if_loop.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int num ;
+    printf("Enter a number less than 10 ");
+    scanf("%d" , &num);
+
+    if(num<=10)
+        printf("What an Obedient servant you are !");
+    
+    else 
+        printf("What a disobedient guy you are !");       
+}
